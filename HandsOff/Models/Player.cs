fff8bae58f58b426748b6e41a920bf4e8f14ab65
Soner Mulder <Sonermulder@gmail.com>
2021-03-24 17:10:19 +0100

    Added constant for BaseStat

--- HandsOff/Models/Player.cs
+++ HandsOff/Models/Player.cs
@@ -8,18 +8,20 @@ namespace HandsOff.Models
 {
     class Player
     {
-        public int Number { get; set; }
-        public string Possition { get; set; }
-        public bool HasBall { get; set; }
+        private const int BaseStat = 55;
+
+        public int Number           { get; set; }
+        public string Possition     { get; set; }
+        public bool HasBall         { get; set; }
 
         /// Player Skills are based on a scale of 1 - 99
-        public int Pace { get; set; } =55;
-        public int Shooting { get; set; } = 55;
-        public int Passing { get; set; } = 55;
-        public int Dribble { get; set; } = 55;
-        public int Defence { get; set; } = 55;
-        public int Intelligence { get; set; } = 55; // Intelligence improves decision making. Where to pass the ball to, when to shoot.
-        public int positition { get; set; } = 0; // this refers to the role of the player; players 1-4 defenders, 4, 5, 6, are midfielders, and 7, 8, 10 are attackers
+        public int Pace             { get; set; } = BaseStat;
+        public int Shooting         { get; set; } = BaseStat;
+        public int Passing          { get; set; } = BaseStat;
+        public int Dribble          { get; set; } = BaseStat;
+        public int Defence          { get; set; } = BaseStat;
+        public int Intelligence     { get; set; } = BaseStat; // Intelligence improves decision making. Where to pass the ball to, when to shoot.
+        public int positition       { get; set; } // this refers to the role of the player; players 1-4 defenders, 4, 5, 6, are midfielders, and 7, 8, 10 are attackers
 
         public Player(int Number, string Possition, int Pace, int Shooting, int Passing, int Dribble, int Defence, int Intelligence)
         {
