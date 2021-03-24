ab78f16cc1ddb20cd373e83e6fd0549749339bff
Soner Mulder <Sonermulder@gmail.com>
2021-03-24 09:29:30 +0100

    Added CheckIfTeamIsComplete() Method

--- HandsOff/Models/Team.cs
+++ HandsOff/Models/Team.cs
@@ -9,9 +9,11 @@ namespace HandsOff.Models
     class Team
     {
         public String TeamName { get; set; }
-        public Player[] Players { get; set; }
+        public Player[] Players { get; set; } // Array of Player, should not exceed 10 players/
 
-
+        /// <summary>
+        /// Sets a "Random" team name.
+        /// </summary>
         public void GetRandomTeamName()
         {
             string[] TeamNames = { "Ajax", "Feyenoord", "PSV", "FC Emmen", "Manchester United", "Chelsea", "AZ", "Mongo Thierry", "Fc MusicMixer", "LTC Assen 6", "Mannen van het zesde", "FC Barcelona", "Tiri Boys", "VVJ Judas", "FcG", "C# Masters", "UWP 4 Life", "Fc Frenkie", "VV Baptist", "James Blunt's Boys", "Ltjes Rozenwater", "Fc Gaan met Die Banaan", "Oranje", "Blauw", "Rood Wit", "Jong Ajax", "GroenGeel", "OranjeRood"};            
@@ -19,6 +21,29 @@ namespace HandsOff.Models
             string RandomTeamName = TeamNames[random.Next(0, TeamName.Length)];
             Console.WriteLine("Random team name = "+RandomTeamName + ".");
             this.TeamName = RandomTeamName;
+        }
+
+        /// <summary>
+        /// Check if team has 10 players and a team name
+        /// </summary>
+        /// <returns></returns>
+        public bool CheckIfTeamIsComplete()
+        {
+            if (this.Players.Length == 10)
+            {
+                if (this.TeamName != null)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
