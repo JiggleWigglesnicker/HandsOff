3c2afd44ae2cbebacbc1835d9c9ccb54a96dc5f4
justin <games.j@live.nl>
2021-04-03 21:59:16 +0200

    Properly multi threaded the GUI generation
    Properly multi threaded the GUI generation

--- HandsOff/SimulationExecution.xaml.cs
+++ HandsOff/SimulationExecution.xaml.cs
@@ -33,90 +33,99 @@ namespace HandsOff
 
         public void CreateMatchListUI()
         {
-            StackPanel stackpanel1 = this.MatchList1;
-            StackPanel stackpanel2 = this.MatchList2;
-            StackPanel stackpanel3 = this.MatchList3;
-
-            int i = 1;
-            int x = 0;
-            foreach (Match match in matches)
+            Task.Run(() =>
             {
-                
-                ListView listView = new ListView();
-                listView.IsHitTestVisible = false;
-                listView.HorizontalAlignment = HorizontalAlignment.Center;
-                listView.Width = 350;
+                StackPanel stackpanel1 = this.MatchList1;
+                StackPanel stackpanel2 = this.MatchList2;
+                StackPanel stackpanel3 = this.MatchList3;
 
-                TextBlock textBlock1 = new TextBlock();
-                textBlock1.Foreground = new SolidColorBrush(Colors.White);
-                textBlock1.FontSize = 45;
-                textBlock1.HorizontalAlignment = HorizontalAlignment.Center;
-                textBlock1.Text = "Match" + i;
-                textBlock1.Height = 50;
-                textBlock1.Width = 200;
-                textBlock1.Margin = new Thickness(0, 40, 0, 0);
+                int i = 1;
+                int x = 0;
 
-                TextBlock textBlock2 = new TextBlock();
-                textBlock2.Foreground = new SolidColorBrush(Colors.White);
-                textBlock2.FontSize = 15;
-                textBlock2.HorizontalAlignment = HorizontalAlignment.Center;
-                textBlock2.Text = match.team1.TeamName + " VS " + match.team2.TeamName;
-                textBlock2.Height = 50;
-                textBlock2.Width = 250;
-                textBlock2.Margin = new Thickness(0, 5, 0, -35);
-
-                TextBlock textBlock3 = new TextBlock();
-                textBlock3.Foreground = new SolidColorBrush(Colors.White);
-                textBlock3.FontSize = 35;
-                textBlock3.HorizontalAlignment = HorizontalAlignment.Center;
-                textBlock3.Text = "Progress";
-                textBlock3.Height = 50;
-                textBlock3.Width = 250;
-                textBlock3.Margin = new Thickness(0, 0, 0, 0);
-
-                ProgressBar progressB = new ProgressBar();
-                progressB.Name = "ProgressBar" + i;
-                progressB.IsIndeterminate = false;
-                progressB.Minimum = 0;
-                progressB.Maximum = 5000000;
-                progressB.Height = 50;
-                progressB.Width = 250;
-                progressB.Margin = new Thickness(0, 0, 0, 0);
-                i++;
-
-
-                switch (x)
+                Parallel.ForEach(matches, async match =>
                 {
-                    case 1:
-                        stackpanel2.Children.Add(listView);
-                        listView.Items.Add(textBlock1);
-                        listView.Items.Add(textBlock2);
-                        listView.Items.Add(textBlock3);
-                        listView.Items.Add(progressB);
-                        x++;
-                        break;
-                    case 2:
-                        stackpanel3.Children.Add(listView);
-                        listView.Items.Add(textBlock1);
-                        listView.Items.Add(textBlock2);
-                        listView.Items.Add(textBlock3);
-                        listView.Items.Add(progressB);
-                        x++;
-                        break;
-                    default:
-                        x = 0;
-                        stackpanel1.Children.Add(listView);
-                        listView.Items.Add(textBlock1);
-                        listView.Items.Add(textBlock2);
-                        listView.Items.Add(textBlock3);
-                        listView.Items.Add(progressB);
-                        x++;
-                        break;
-                }
+                    await Dispatcher.RunAsync(CoreDispatcherPriority.High, () =>
+                    {
+                        ListView listView = new ListView();
+                        listView.IsHitTestVisible = false;
+                        listView.HorizontalAlignment = HorizontalAlignment.Center;
+                        listView.Width = 350;
 
-            }
+                        TextBlock textBlock1 = new TextBlock();
+                        textBlock1.Foreground = new SolidColorBrush(Colors.White);
+                        textBlock1.FontSize = 45;
+                        textBlock1.HorizontalAlignment = HorizontalAlignment.Center;
+                        textBlock1.Text = "Match" + i;
+                        textBlock1.Height = 50;
+                        textBlock1.Width = 200;
+                        textBlock1.Margin = new Thickness(0, 40, 0, 0);
 
-            BigBar.Maximum = matches.Count();
+                        TextBlock textBlock2 = new TextBlock();
+                        textBlock2.Foreground = new SolidColorBrush(Colors.White);
+                        textBlock2.FontSize = 15;
+                        textBlock2.HorizontalAlignment = HorizontalAlignment.Center;
+                        textBlock2.Text = match.team1.TeamName + " VS " + match.team2.TeamName;
+                        textBlock2.Height = 50;
+                        textBlock2.Width = 250;
+                        textBlock2.Margin = new Thickness(0, 5, 0, -35);
+
+                        TextBlock textBlock3 = new TextBlock();
+                        textBlock3.Foreground = new SolidColorBrush(Colors.White);
+                        textBlock3.FontSize = 35;
+                        textBlock3.HorizontalAlignment = HorizontalAlignment.Center;
+                        textBlock3.Text = "Progress";
+                        textBlock3.Height = 50;
+                        textBlock3.Width = 250;
+                        textBlock3.Margin = new Thickness(0, 0, 0, 0);
+
+                        ProgressBar progressB = new ProgressBar();
+                        progressB.Name = "ProgressBar" + i;
+                        progressB.IsIndeterminate = false;
+                        progressB.Minimum = 0;
+                        progressB.Maximum = 5000000;
+                        progressB.Height = 50;
+                        progressB.Width = 250;
+                        progressB.Margin = new Thickness(0, 0, 0, 0);
+                        i++;
+
+
+                        switch (x)
+                        {
+                            case 1:
+                                stackpanel2.Children.Add(listView);
+                                listView.Items.Add(textBlock1);
+                                listView.Items.Add(textBlock2);
+                                listView.Items.Add(textBlock3);
+                                listView.Items.Add(progressB);
+                                x++;
+                                break;
+                            case 2:
+                                stackpanel3.Children.Add(listView);
+                                listView.Items.Add(textBlock1);
+                                listView.Items.Add(textBlock2);
+                                listView.Items.Add(textBlock3);
+                                listView.Items.Add(progressB);
+                                x++;
+                                break;
+                            default:
+                                x = 0;
+                                stackpanel1.Children.Add(listView);
+                                listView.Items.Add(textBlock1);
+                                listView.Items.Add(textBlock2);
+                                listView.Items.Add(textBlock3);
+                                listView.Items.Add(progressB);
+                                x++;
+                                break;
+
+
+                        }
+                        BigBar.Maximum = matches.Count();
+                    });
+                });
+
+
+                
+            });
         }
 
         public async void StartExecution()
